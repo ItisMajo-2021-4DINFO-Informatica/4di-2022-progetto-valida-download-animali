b32507a68443e6ddefab61ebf8f823a75e687c10
Jacopo Favat <88483635+JacopoFavat@users.noreply.github.com>
2022-03-23 13:30:55 +0100

    Codice Versione 1.1
    In questa Versione rispetto alla precedente, abbiamo aggiunto l'immagine di sfondo e immesso la libreria :"System.Security.Cryptography;"

--- 03-codice/AnimaliValidaDownloadApp/AnimaliValidaDownloadApp/MainWindow.xaml.cs
+++ 03-codice/AnimaliValidaDownloadApp/AnimaliValidaDownloadApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
 
 namespace AnimaliValidaDownloadApp
 {
@@ -24,5 +25,8 @@ namespace AnimaliValidaDownloadApp
         {
             InitializeComponent();
         }
+
+
     }
+
 }
