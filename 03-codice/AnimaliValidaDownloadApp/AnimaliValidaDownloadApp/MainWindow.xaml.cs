f70045bc0aad56fabf80b4328676ba95b206e070
Jacopo Favat <88483635+JacopoFavat@users.noreply.github.com>
2022-03-24 12:45:16 +0100

    Codice Versione 1.5
    In questa Versione rispetto alla precedente, abbiamo corretto un bug e modificato la forma del bottone

--- 03-codice/AnimaliValidaDownloadApp/AnimaliValidaDownloadApp/MainWindow.xaml.cs
+++ 03-codice/AnimaliValidaDownloadApp/AnimaliValidaDownloadApp/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Security.Cryptography;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
-using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Windows;
 
 namespace AnimaliValidaDownloadApp
 {
@@ -28,20 +16,22 @@ namespace AnimaliValidaDownloadApp
             InitializeComponent();
         }
 
-    
-        private void OpenFile_Click(object sender, RoutedEventArgs e)
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)               //CODICE FILESYSTEM
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
+            if (openFileDialog.ShowDialog() == true) ;
+              
+            if (MessageBox.Show("Vuoi Importare Questo File?",
+                   "Save file",
+                   MessageBoxButton.YesNo,
+                   MessageBoxImage.Question) == MessageBoxResult.Yes)
                 txtFile.Text = File.ReadAllText(openFileDialog.FileName);
-             if (MessageBox.Show("Vuoi Importare Questo File?",
-                    "Save file",
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Question) == MessageBoxResult.Yes)
-{
-    // Do something here
-}
-        }
+            {
+                // l'utente fa qualocsa
+            }
+        }                                                                            //FINE CODICE FILESYSTEM
+
     }
 }
 
